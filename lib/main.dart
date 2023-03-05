a918590e3cc09ff8440947c6cfb4802593a34f0d
BMisheto <79095816+BMisheto@users.noreply.github.com>
2023-03-05 13:05:31 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,70 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:provider/provider.dart';
 import 'package:sema/common/bottom_bar.dart';
-import 'package:sema/features/auth/screens/login_screen.dart';
-import 'package:sema/features/auth/services/auth_service.dart';
-import 'package:sema/providers/user_provider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(
-      create: (context) => UserProvider(),
-    ),
-  ], child: const MyApp()));
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final AuthService authService = AuthService();
-
-  @override
-  void initState() {
-    super.initState();
-    authService.getUserData(context);
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Sema',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // scaffoldBackgroundColor: Colors.white,
-        // colorScheme: const ColorScheme.dark(
-        //   primary: Colors.white,
-        // ),
-        appBarTheme: AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-        ),
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      home: (Provider.of<UserProvider>(context).user?.token.isNotEmpty == true
-          ? const BottomBar()
-          : const AuthScreen()),
+      home: const MyHomePage(title: 'Sema'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+  
+      body: Center(
+
+        child: BottomBar(),
+        
+      ),
+   
     );
   }
 }
